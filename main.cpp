ef89c410fd64ae024eb66402cedb78fe783faa7a
414645 <414645@bsd48.org>
2022-11-29 14:15:34 -0800

    2:15 in c++ (most of main)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "media.h"
+#include <cstring>
 
 using namespace std;
 
@@ -71,15 +72,106 @@ int main() {
 
 void addMedia()
 {
+      cin.get(input, 7); //6 chars
+    cin.ignore(80,'\n');
+    
+    //check for each command and run the correct function
+    if (input[0] == 'A' &&
+	input[1] == 'D' &&
+	input[2] == 'D' )
+      {
+	addMedia();
+      }
+    else if (input[0] == 'S' &&
+	     input[1] == 'E' &&
+	     input[2] == 'A' &&
+	     input[3] == 'R' &&
+	     input[4] == 'C' &&
+	     input[5] == 'H')
+      {
+	searchMedia();
+      }
+    else if (input[0] == 'D' &&
+	     input[1] == 'E' &&
+	     input[2] == 'L' &&
+	     input[3] == 'E' &&
+	     input[4] == 'T' &&
+	     input[5] == 'E' )
+      {
+	deleteMedia();
+      }
+    else if (input[0] == 'Q' &&
+	     input[1] == 'U' &&
+	     input[2] == 'I' &&
+	     input[3] == 'T' )
+      {
+	quit = true;
+      }
+    else
+      {
+	cout << "Cannot find that command" << endl;
+	cout << "The possible commands are: " <<endl;
+	cout << "ADD" << endl;
+	cout << "SEARCH" << endl;
+	cout << "DELETE" << endl;
+	cout << "QUIT" << endl;
+      }
+  }
+  
+}
 
+void addMedia()
+{
+  cout << "If you would like to add a Video Game type 1" << endl;
+  cout << "If you would like to add a Song(Music) type 2" << endl;
+  cout << "If you would like to add a Movie type 3" << endl;
+  int number = 0;
+  cin >> number;
+  if (number == 1)
+    {
+      //title
+      //year
+      //publisher
+      //rating
+    }
+  else if (number == 2)
+    {
+      //title
+      //artist
+      //year
+      //duration
+      //publisher
+    }
+  else if (number == 3)
+    {
+      //title
+      //director
+      //year
+      //duration
+      //rating
+    }
+  
+  /*
+  char title[6];
+  for (int a = 0; a < 6; a++)
+    {
+      title[a] = ' ';
+    }
+    cin.get(title, 7); //6 chars
+    cin.ignore(80,'\n');
+  */
 }
 
 void searchMedia()
 {
-
-}
+  //search by title or year? (if statement)
+  //get search term(cin)
+  //go though the vector/list/pointers with the given criteria
+} 
 
 void deleteMedia()
 {
-
+  //search as above
+  //double check on selected resultes
+  //call a deconstructor do destroy everything!
 }
