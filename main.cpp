7388fc79dbcec8b6cf46fc4e275da2e42401bcf2
414645 <414645@bsd48.org>
2022-12-01 17:43:28 -0800

    it was not the new code, new code still does not work but the proble was i flipped the >> for cin, also I was not updating main on git

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include "media.h"
 #include "videogame.h"
+#include <vector>
 
 #include <cstring>
 
 using namespace std;
 
-void addMedia();
-void searchMedia();
-void deleteMedia();
+void addMedia(vector<Media> &mediaList);
+void searchMedia(vector<Media> &mediaList);
+void deleteMedia(vector<Media> &mediaList);
 //void quit(); just quit
 
 int main() {
@@ -21,19 +22,23 @@ int main() {
       input[a] = ' ';
     }
   bool quit = false;
+
+  //media*
+  vector<Media> mediaList;
   
-  //rest of code
+  //User input, what do you want to do?
   while (quit == false) {
     //cin.ignore(80,'\n'); 
-    cin.get(input, 7); //6 chars
-    cin.ignore(80,'\n');
+    //cin.get(input, 7); //6 chars
+    //cin.ignore(80,'\n');
+    cin >> input;
     
     //check for each command and run the correct function
     if (input[0] == 'A' &&
 	input[1] == 'D' &&
 	input[2] == 'D' )
       {
-	addMedia();
+	addMedia(mediaList);
       }
     else if (input[0] == 'S' &&
 	     input[1] == 'E' &&
@@ -42,7 +47,7 @@ int main() {
 	     input[4] == 'C' &&
 	     input[5] == 'H')
       {
-	searchMedia();
+	searchMedia(mediaList);
       }
     else if (input[0] == 'D' &&
 	     input[1] == 'E' &&
@@ -51,7 +56,7 @@ int main() {
 	     input[4] == 'T' &&
 	     input[5] == 'E' )
       {
-	deleteMedia();
+	deleteMedia(mediaList);
       }
     else if (input[0] == 'Q' &&
 	     input[1] == 'U' &&
@@ -62,6 +67,7 @@ int main() {
       }
     else
       {
+	//they entered a invalid command, display comands for them
 	cout << "Cannot find that command" << endl;
 	cout << "The possible commands are: " <<endl;
 	cout << "ADD" << endl;
@@ -73,70 +79,136 @@ int main() {
   
 }
 
-void addMedia()
+void addMedia(vector<Media> &mediaList)
 {
+  //variables needed (yes they could be resused but speed is not a problem)
+  int number = 0;
+  char title[80];
+  int year;
+  char publisher[80];
+  char rating[80];
+  char artist[80];
+  char duration[80]; //char since ##:##
+  char director[80];
+  
+  //What type of media
   cout << "If you would like to add a Video Game type 1" << endl;
   cout << "If you would like to add a Song(Music) type 2" << endl;
   cout << "If you would like to add a Movie type 3" << endl;
-  int number = 0;
   cin >> number;
-  if (number == 1)
+
+  //default media
+  cout << "please enter the title of the videogame" << endl;
+  cin >> title;
+  //cin.get(title, 81); //80 chars
+  //cin.ignore(80,'\n');
+  
+  cout << "please enter the year the videogame was made" << endl;
+  cin >> year;
+
+  //the folowing could be reorgainsed so each type of meadia has it's own if
+  //for some reason that format annoyed me so...
+  //reminder: 1 is vedio game, 2 is music, 3 is movie
+
+  //publisher 1,2
+  if (number == 1 || number == 2)
     {
-      char title[80];
-      int year;
-      char publisher[80];
-      char rating[80]; 
-      cout << "please enter the title of the videogame" << endl;
-      cin >> title;
-      cout << "please enter the year  the videogame was made" << endl;
-      cin >> year;
-      cout << "please enter the publisher of the videogame" << endl;
+      cout << "please enter the publisher" << endl;
       cin >> publisher;
-      cout << "please enter the rating of the videogame" << endl;
-      cin >> rating; 
-      
-      Videogame* newchild = new Videogame(title, year, publisher, rating);
-      //title
-      //year
-      //publisher
-      //rating
-    }
-  else if (number == 2)
-    {
-      //title
-      //artist
-      //year
-      //duration
-      //publisher
-    }
-  else if (number == 3)
-    {
-      //title
-      //director
-      //year
-      //duration
-      //rating
     }
   
-  /*
-  char title[6];
-  for (int a = 0; a < 6; a++)
+  //rating 1,3
+  if (number == 1 || number == 3)
     {
-      title[a] = ' ';
+      cout << "please enter the rating" << endl;
+      cin >> rating;
     }
-    cin.get(title, 7); //6 chars
-    cin.ignore(80,'\n');
-  */
+  
+  //artist 2
+  if (number == 2)
+    {
+      cout << "please enter the artist" << endl;
+      cin >> artist;
+    }
+  
+  //duration 2,3
+  if (number == 2 || number == 3)
+    {
+      cout << "please enter the duration" << endl;
+      cin >> duration;
+    }
+  
+  //director 3
+  if (number == 3)
+    {
+      cout << "please enter the director" << endl;
+      cin >> director;
+    }
+
+  //make the information into a class
+  if (number == 1) {
+    //title, year, publisher, rating
+    Videogame* newchild = new Videogame(title, year, publisher, rating);
+    mediaList.push_back(*newchild);
+    //add it to vector now since otherwise it is out of scope
+  }
+  else if (number == 2) {
+    //title, artist, year, duration, publisher
+    //Videogame* newchild = new Videogame(title, year, publisher, rating);
+  }
+  else if (number == 3) {
+    //title, director, year, dration, rating
+    //Videogame* newchild = new Videogame(title, year, publisher, rating);
+  }
+  
+  //want add the newly created class to the vector
+  //exept we had to do it earlier so it is not out of scope
+  //mediaList.push_back(*newchild);
 }
 
-void searchMedia()
+void searchMedia(vector<Media> &mediaList)
 {
   //search by title or year? (if statement)
   //get search term(cin)
-  //go though the vector/list/pointers with the given criteria
+  //go though the vector with the given criteria
+  
+  //needed variables
+  int number = 0;
+  char title[80];
+  int year;
+  
+  
+  //Title or Year?
+  //also coding reminder do not flip the << (>> is bad)
+  cout << "If you would like to search by title type 1" << endl;
+  cout << "If you would like to search by year type 2" << endl;
+  
+  cin >> number;
+  
+  if (number = 1) {
+    cout << "Please type the title you are looking for" << endl;
+    cin >> title;
+  }
+  else if (number == 2) {
+    cout << "Please type the year you are looking for" << endl;
+    cin >> year;
+  }
+  
+  for (int a = 0; a < mediaList.size(); a++) {
+    if (number = 1) {
+      //if ((mediaList[a]->getTitle()) == title) {
+      //	cout << "sucess " << mediaList[a]->getTitle() << endl;
+      //}
+    }
+    else if (number == 2) {
+      //if (mediaList[a]->getYear() == year) {
+      //	cout << "yay" << endl;
+      //}
+    }
+  }
 } 
 
-void deleteMedia()
+void deleteMedia(vector<Media> &mediaList)
 {
   //search as above
   //double check on selected resultes
